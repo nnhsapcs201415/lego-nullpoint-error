9f0b356318f7a3492f774ff461c650c938646c75
Jonathan Lowe <jjlowe@stu.naperville203.org>
2017-05-17 15:09:59 -0500

    Worked on light sensor listener

--- LegoMindstorms/LS.java
+++ LegoMindstorms/LS.java
@@ -4,10 +4,15 @@ import lejos.robotics.navigation.*;
 public class LS implements SensorPortListener{
     
     public static void main (String args[]){
-        SensorPort.S2.addSensorPortListener(this);
+        LS listener = new LS();
+        LightSensor light = new LightSensor(SensorPort.S2);
+        SensorPort.S2.addSensorPortListener(listener);
     }
     
     public void stateChanged(SensorPort port, int value, int oldValue){
-        if(port == SensorPort.S1 
+        if(port == SensorPort.S2){
+            LCD.drawInt(light.readValue(),0,0);
+            Button.waitForAnyPress();
+        }
     }
 }
