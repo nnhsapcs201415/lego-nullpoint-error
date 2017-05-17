53ef08cfa09fc57ec45f6e3b6fddefa04e419770
Jonathan Lowe <jjlowe@stu.naperville203.org>
2017-05-17 15:11:06 -0500

    Finished Light Sensor

--- LegoMindstorms/LS.java
+++ LegoMindstorms/LS.java
@@ -7,11 +7,11 @@ public class LS implements SensorPortListener{
         LS listener = new LS();
         LightSensor light = new LightSensor(SensorPort.S2);
         SensorPort.S2.addSensorPortListener(listener);
+        LCD.drawInt(light.readValue(),0,0);
     }
     
     public void stateChanged(SensorPort port, int value, int oldValue){
         if(port == SensorPort.S2){
-            LCD.drawInt(light.readValue(),0,0);
             Button.waitForAnyPress();
         }
     }
