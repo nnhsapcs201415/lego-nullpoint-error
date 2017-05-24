a9a149943cd78f5dc2e2b4b205088bb116b837c3
Jonathan Lowe <jjlowe@stu.naperville203.org>
2017-05-24 11:08:11 -0500

    Light detection for challenge works, almost got out of box working

--- Practical/Challenge.java
+++ Practical/Challenge.java
@@ -5,6 +5,7 @@ import lejos.robotics.objectdetection.*;
 import lejos.robotics.Touch;
 public class Challenge implements SensorPortListener, FeatureListener {
     public static int MAX_DETECT = 80;
+    public static int BOX = 15;
     UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
     Touch ts = new TouchSensor(SensorPort.S3);
     LightSensor ls = new LightSensor(SensorPort.S2);
@@ -12,44 +13,57 @@ public class Challenge implements SensorPortListener, FeatureListener {
     int state = 0;
     public Challenge() throws Exception, InterruptedException {
         RangeFeatureDetector rd = new RangeFeatureDetector(us, MAX_DETECT, 500);
-        rd.addListener(this);
         FeatureDetector fd = new TouchFeatureDetector(ts);
+        rd.addListener(this);
         fd.addListener(this);
         SensorPort.S2.addSensorPortListener(this);
     }
+
     public static void main(String args[]) throws Exception{
         Challenge c = new Challenge();
         Button.waitForAnyPress();
-        c.pilot.travel(50, true);
+        c.pilot.setTravelSpeed(30);
         c.run();
-        
     }
+
     public void featureDetected(Feature feature, FeatureDetector detector) {
-        
-    }
-    public void stateChanged(SensorPort port, int value, int oldValue){
-        if( oldValue > 780 ){
-            state = 3;
+        int range = (int)feature.getRangeReading().getRange();
+        LCD.drawInt(range,0,3);
+        if(range < 20){
+            state = 0;
         }
     }
-    public void run(){
+    public void stateChanged(SensorPort port, int value, int oldValue){
+        if(port == SensorPort.S2){
+            LCD.drawInt(value,0,2);
+            if( value > 700){
+                state = 3;
+            }
+        }
+    }
+
+    public void run() throws InterruptedException{
         while(true){
             if(state == 0){
                 //in box
-                
+                LCD.drawString("State 0", 0, 1);
+                pilot.setRotateSpeed(10);
+                pilot.rotate(15, true);
             }
             if(state == 1){
                 //look for cans
-                
+                LCD.drawString("State 1", 0, 1);
+                pilot.travel(50,false);
             }
             if(state == 2){
                 //push can
-                
+                LCD.drawString("State 2", 0, 1);
             }
             if(state == 3){
                 //hit line, backup
-                Motor.A.rotate(180);
-                pilot.travel(50, false);
+                LCD.drawString("State 3", 0, 1);
+                pilot.travel(-15, false);
+                pilot.rotate(180, false);
             }
         }
     }
